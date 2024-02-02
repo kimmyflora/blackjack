cc78993ea04ca6d0ba1592e005b83af2854ce398
User Name <kimmyflora270@gmail.com>
2024-02-02 12:38:15 -0800

    added read me

---
+++ js/app.js
@@ -0,0 +1,42 @@
+  /*----- constants -----*/
+
+const cards = {
+
+}
+
+  /*----- state variables -----*/
+let scores // 
+let 
+let winner // who won that round 
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+    // hit button
+    //stand button 
+    //bet button 
+
+  /*----- functions -----*/
+
+initalize()
+
+function initalize(){
+
+}
+
+
+function render(){
+    renderScores()
+    render
+}
+
+function getWinner(){
+    
+}
+
+
+
+function rndCards(){
+
+}
